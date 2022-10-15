831e633604b77311c588551f11e32d84c17bde8e
USER <USER@DESKTOP-TRQG01D>
2022-10-15 13:42:56 +0300

    Добавьте файлы проекта.

---
+++ Pr.8.Zd.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Hello
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите значение координаты x=");
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("Введите значение координаты y=");
+            float y = float.Parse(Console.ReadLine());
+            if (-23<x && x<0 && x<y && y<0 )
+                Console.WriteLine("Точка (x,y) лежит внутри закрашенной области");
+            else if (-23>x || x>0 || y>0 )
+                Console.WriteLine("Точка (x,y) лежит вне закрашенной области");
+            else Console.WriteLine("Точка (x,y) лежит на границе закрашенной области");
+        }
+    }
+}
